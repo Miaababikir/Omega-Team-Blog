a5e1341ca9d87747feb7cfeae6fa694d3380b139
Miaababikir <miaababikir@gmail.com>
2019-04-03 22:17:24 +0200

    Adding navbar

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,6 @@
+<div class="w-full m-0 p-0 bg-cover bg-bottom" style="background-image: url('/images/cover.jpg'); height: 60vh; max-height: 460px; ">
+    <div class="container mx-auto text-center pt-32">
+        <p class="text-white font-extrabold  text-5xl mb-2">👻 Omega Team Co.</p>
+        <p class="text-grey text-2xl">Welcome to our blog</p>
+    </div>
+</div>
---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,13 @@
+<div class="flex justify-between w-full h-12 p-3 text-white items-center">
+    <ul class="list-reset flex">
+        <li class="mr-6"><a class="text-white no-underline hover:text-white" href="#">Home</a></li>
+        <li class="mr-6"><a class="text-grey no-underline hover:text-white" href="#">Posts</a></li>
+        <li class="mr-6"><a class="text-grey no-underline hover:text-white" href="#">Contact</a></li>
+        <li class="mr-6"><a class="text-grey no-underline hover:text-white" href="#">About</a></li>
+    </ul>
+
+    <ul class="list-reset flex">
+        <li class="mr-6"><a class="text-grey no-underline hover:text-blue-dark" href="#"><i class="fab fa-facebook-f"></i></a></li>
+        <li class="mr-6"><a class="text-grey no-underline hover:text-teal" href="#"><i class="fab fa-twitter"></i></a></li>
+    </ul>
+</div>
